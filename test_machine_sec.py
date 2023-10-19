b87bbb151bd27b8dab007f56cefa1c2817975b6e
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-10-19 22:13:11 +0200

    chore: first cleanup of test folder

--- test_machine_sec.py
+++ test_machine_sec.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         columns=["urineoutput", "weight", "crrt_status"]
     ).reset_index()
     weight = data.drop(columns=["urineoutput", "creat", "crrt_status"]).reset_index()
-    crrt = data.drop(columns=["urineoutput", "creat", "weight"]).reset_index()
+    # crrt = data.drop(columns=["urineoutput", "creat", "weight"]).reset_index()
     # prep weights
     weight = weight.drop_duplicates(subset=["weight"], keep="first").reset_index()
 
@@ -21,8 +21,10 @@ if __name__ == "__main__":
             pyAKI.utils.Dataset(pyAKI.utils.DatasetType.URINEOUTPUT, urine_output),
             pyAKI.utils.Dataset(pyAKI.utils.DatasetType.CREATININE, creatinine),
             pyAKI.utils.Dataset(pyAKI.utils.DatasetType.DEMOGRAPHICS, weight),
-            pyAKI.utils.Dataset(pyAKI.utils.DatasetType.CRRT, crrt),
-        ]
+            # pyAKI.utils.Dataset(pyAKI.utils.DatasetType.CRRT, crrt),
+        ],
     )
-    ana.process_stays().to_csv("tests/experiment/data/test_machine_aki_second.csv")
-    ana.process_stays().to_excel("tests/experiment/data/test_machine_aki_second.xlsx")
+    ana.process_stays().to_csv("tests/experiment/data/test_machine_aki_second_XXX.csv")
+    ana.process_stays().to_excel(
+        "tests/experiment/data/test_machine_aki_second_XXX.xlsx"
+    )
