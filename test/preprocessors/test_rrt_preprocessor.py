0e291e86d5edd0b95100283743dc15c68f382558
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-11-12 21:27:43 +0100

    add rrt preprocessor test

--- test/preprocessors/test_rrt_preprocessor.py
+++ test/preprocessors/test_rrt_preprocessor.py
@@ -2,11 +2,46 @@ from unittest import TestCase
 import pandas as pd
 
 from pyAKI.preprocessors import RRTPreProcessor
+from pyAKI.utils import Dataset, DatasetType
 
 
 class TestRRTPreProcessor(TestCase):
     def setUp(self) -> None:
-        pass
+        self.preprocessor = RRTPreProcessor()
 
     def test_preprocessor(self):
-        pass
+        rrt_df = pd.DataFrame(
+            data={
+                "stay_id": [1, 1, 1],
+                "rrt_status": [0, 1, pd.NA],
+            },
+            index=pd.to_datetime(
+                [
+                    "2023-01-01 00:00:00",
+                    "2023-01-01 15:00:00",
+                    "2023-01-01 23:00:00",
+                ]
+            ),
+        )
+
+        _, df = self.preprocessor.process([Dataset(DatasetType.RRT, rrt_df)])[0]
+
+        pd.testing.assert_series_equal(
+            df["rrt_status"],
+            pd.Series(
+                data=[0] * 15 + [1] * 9,
+                name="rrt_status",
+                index=pd.MultiIndex.from_arrays(
+                    [
+                        [1] * 24,
+                        pd.period_range(
+                            start="2023-01-01 00:00:00",
+                            end="2023-01-01 23:00:00",
+                            freq="h",
+                        ),
+                    ],
+                    names=("stay_id", ""),
+                ),
+            ),
+            check_index=False,
+        )
